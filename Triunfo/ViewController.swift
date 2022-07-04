849cbcd9c8962cea4f4005950f4a9d2c5a6ff862
Ryan Araújo <ryanaraujo@MacBook-Pro-9.local>
2022-07-04 10:27:20 -0300

    Adiciona projeto do xcode e realiza primeiro commit

---
+++ Triunfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Triunfo
+//
+//  Created by Ryan Araújo on 04/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
